3fca32c7490d027bf77ac4bb88fac55b885672a7
Leslie Ngo <ngo.leslie@gmail.com>
2021-06-19 11:51:58 -0700

    Time: 112 ms (99.60%), Space: 44.5 MB (72.27%) - LeetHub

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -4,9 +4,22 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    let joined = nums1.concat(nums2);
-    
-    joined.sort((a, b) => a - b);
+    let x = 0, y = 0;
+    let joined = [];
+    while(x < nums1.length || y < nums2.length) {
+        if((nums1[x] < nums2[y]) || nums2[y] === undefined) {
+            joined.push(nums1[x]);
+            x++;
+        } else if((nums1[x] > nums2[y]) || nums1[x] === undefined) {
+            joined.push(nums2[y]);
+            y++;
+        } else if(nums1[x] === nums2[y]) {
+            joined.push(nums1[x]);
+            joined.push(nums2[y]);
+            x++;
+            y++;
+        }
+    }
     
     if(joined.length % 2 === 0) {
         return (joined[(joined.length/2) - 1] + joined[(joined.length/2)]) / 2;
