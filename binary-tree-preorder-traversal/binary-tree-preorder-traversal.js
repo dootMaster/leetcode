7852bf5e10d29b0d8fd97fcf4567ea8108e28817
Leslie Ngo <ngo.leslie@gmail.com>
2021-08-27 18:53:47 -0700

    Time: 72 ms (71.66%), Space: 38.7 MB (90.13%) - LeetHub

---
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+var preorderTraversal = function(root) {
+  if (!root) return [];
+  var result = [];
+  var stack = [root];
+  
+  while(stack.length) {
+    var node = stack.pop();
+    result.push(node.val);
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
+  }
+  return result;
+};
