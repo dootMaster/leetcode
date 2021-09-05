b90057ca0b3283b30df044d6bdd1f62ca37a84d2
Leslie Ngo <ngo.leslie@gmail.com>
2021-09-04 23:59:59 -0700

    Time: 72 ms (72.44%), Space: 38.9 MB (33.46%) - LeetHub

---
+++ find-the-highest-altitude/find-the-highest-altitude.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+    let max = 0, sum = 0;
+    
+    for(let i = 0; i < gain.length; i++) {
+        sum += gain[i];
+        if(sum > max) max = sum;
+    }
+    
+    return max;
+};
