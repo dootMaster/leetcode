e3e9b33deba9c968720f8ab42a14ae38d2c65fcc
Leslie Ngo <ngo.leslie@gmail.com>
2022-01-03 00:42:05 -0800

    Time: 68 ms (97.63%), Space: 40.8 MB (23.72%) - LeetHub

---
+++ binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    let result = [];
+    const traverse = (node, level) => {
+        if(result.length === level) result.push([]);
+        result[level].push(node.val);
+
+        if (node.left) traverse(node.left, level + 1);
+        if (node.right) traverse(node.right, level + 1);
+    }
+    
+    if(!root) return result;
+    traverse(root, 0);
+    return result;
+};
