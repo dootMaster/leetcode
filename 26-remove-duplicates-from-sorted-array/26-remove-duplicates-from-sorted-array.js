aef6d707580f64d66467720ab8227b9f20fc5da3
Leslie Ngo <ngo.leslie@gmail.com>
2022-03-01 19:00:25 -0800

    Time: 84 ms (86.29%), Space: 44.8 MB (31.68%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -2,19 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-const removeDuplicates = function(nums) {
-    if(nums.length === 1) return nums.length;
-    
+var removeDuplicates = function(nums) {
     let i = 0;
-    let j = 1
-    while(i < nums.length && j < nums.length) {
-        if (nums[j] === nums[i]) {
+    let j = 1;
+    let l = 1;
+    while(j < nums.length) {
+        if(nums[j] === nums[i]) {
             j++;
-        }    
-        else {
-            nums[i+1] = nums[j]
-            i++
-        }    
+        } else {
+            nums[i + 1] = nums[j];
+            i++;
+            l++;
+        }
     }
-    return i + 1
+    return l;
 };
